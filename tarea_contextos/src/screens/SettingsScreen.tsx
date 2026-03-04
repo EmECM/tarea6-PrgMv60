fb388cc4c9dbf514f8bfd1d382dc93516f94226a
Emerson Caballero <emersoncaballero2017@gmail.com>
2026-03-04 12:40:09 -0600

    - llamada a la funcion del hook - estilizacion de boton "cerrar sesion" - integracion de stylesheet para el contenedor

--- tarea_contextos/src/screens/SettingsScreen.tsx
+++ tarea_contextos/src/screens/SettingsScreen.tsx
@@ -1,12 +1,40 @@
-import {Text} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
+import { useAuth } from "../components/contexts/AuthContext";
+import CustomButton from "../components/CustomButton";
 
-const SettingsScreen = () => {
+export default function SettingsScreen(){
+    //llamada a la funcion del hook
+    const {logout} = useAuth();
+
     return(
-        <Text>
-            Bienvenido al area de configuracion!
-        </Text>
+<View style={styles.container}>
+      <Text style={styles.text}>Estas en Settings</Text>
+      
+      {/*boton para cierre de sesion*/}
+      <View style={styles.buttonContainer}>
+        <CustomButton 
+          title="Cerrar Sesión" 
+          onClick={logout} 
+          variant="secondary" 
+        />
+      </View>
+    </View>
     );
     
 }
-
-export default SettingsScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  buttonContainer: {
+    marginTop: 20,
+    width: '60%',
+  },
+});
